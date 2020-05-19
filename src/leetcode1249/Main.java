ea9f456e4e296a6623f0757d8f8c7f66f36a7550
chaninkou <michaelkoulove@yahoo.com>
2020-05-19 00:10:38 -0700

    Great problem on removing ( or ) to make valid parentheses with stack.

--- src/leetcode1249/Main.java
+++ src/leetcode1249/Main.java
@@ -1,5 +1,13 @@
 package leetcode1249;
 
 public class Main {
-
+	public static void main(String[] args) {
+		String s = "lee(t(c)o)de)";
+		
+		System.out.println("Input: " + s);
+		
+		RemoveMinParenthesesFunction solution = new RemoveMinParenthesesFunction();
+		
+		System.out.println("Solution: " + solution.minRemoveToMakeValid(s));
+	}
 }
--- src/leetcode1249/RemoveMinParenthesesFunction.java
+++ src/leetcode1249/RemoveMinParenthesesFunction.java
@@ -1,5 +1,97 @@
 package leetcode1249;
 
-public class RemoveMinParenthesesFunction {
+import java.util.Stack;
 
+public class RemoveMinParenthesesFunction {
+  
+    // Use * to remove using regular expression
+    public String minRemoveToMakeValid2(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i = 0; i < sb.length(); i++){
+            if(sb.charAt(i) == '('){
+                stack.push(i);
+            } else if (sb.charAt(i) == ')'){
+                if(!stack.isEmpty()){
+                    stack.pop();
+                } else {
+                    sb.setCharAt(i, '*');
+                }
+            }
+        }
+        
+        while(!stack.isEmpty()){
+            sb.setCharAt(stack.pop(), '*');
+        }
+        
+        return sb.toString().replaceAll("\\*", "");
+    }
+    
+    // Check right to left method without using stack
+    public String minRemoveToMakeValid3(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        int numOfDeletion = 0;
+        
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '('){
+                numOfDeletion++;
+            } else if (s.charAt(i) == ')'){
+            	// If 0, don't add anything
+                if(numOfDeletion == 0){
+                    continue;
+                } else {
+                    numOfDeletion--;
+                }
+            }
+            
+            sb.append(s.charAt(i));
+        }
+        
+        StringBuilder result = new StringBuilder();
+
+        for(int i = sb.length() - 1; i >= 0; i--){
+        	// numOfDeletion will be the number need to be deleted
+            if(sb.charAt(i) == '(' && numOfDeletion > 0){
+                numOfDeletion--;
+                continue;
+            }
+            result.append(sb.charAt(i));
+        }
+        
+        // reverse after then we did add from right to left
+        return result.reverse().toString();
+    }
+    
+    public String minRemoveToMakeValid(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i = 0; i < sb.length(); i++){
+            if(sb.charAt(i) == '('){
+                stack.push(i + 1);
+            } else if (sb.charAt(i) == ')'){
+                if(!stack.isEmpty() && stack.peek() >= 0){
+                    stack.pop();
+                } else {
+                    stack.push(-(i + 1));
+                }
+            }
+        }
+        
+        StringBuilder result = new StringBuilder();
+
+        for(int i = 0, j = 0; i < sb.length(); i++){
+            if(j >= stack.size() || i != Math.abs(stack.elementAt(j)) - 1){
+                result.append(sb.charAt(i));
+            } else {
+                j++;
+            }
+        }
+        
+        return result.toString();
+    }
 }
